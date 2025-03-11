bdd45fb3399d2c488d340ef432ef5bf1a50ab78d
Antoine <antoine.prudhomme@cryptio.co>
2025-03-11 22:14:57 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -16,5 +17,7 @@ fn main() {
     // day2::run_part_1();
     // day2::run_part_2();
     // day3::run_part_1();
-    day3::run_part_2();
+    // day3::run_part_2();
+    // day4::run_part_1();
+    day4::run_part_2();
 }
