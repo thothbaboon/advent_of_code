4e7689021888f568bc1845a715a5505eee0099bf
Antoine <antoine.prudhomme@cryptio.co>
2025-03-14 15:10:27 +0100

    refactor day 6 for part 2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,12 +1,12 @@
 use lazy_static::lazy_static;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::read_input;
 
 const VISITED_CHAR: char = 'X';
 const OBSTACLE_CHAR: char = '#';
 
-#[derive(Hash, PartialEq, Eq, Clone)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
 enum Direction {
     North,
     South,
@@ -29,6 +29,7 @@ lazy_static! {
     ]);
 }
 
+#[derive(Hash, PartialEq, Eq, Copy, Clone)]
 struct Position {
     pub row: i32,
     pub col: i32,
@@ -56,14 +57,6 @@ impl From<(i32, i32)> for Position {
 struct Grid(Vec<Vec<char>>);
 
 impl Grid {
-    fn mark_cell_as_visited_if_not(&mut self, position: &Position) -> bool {
-        if self.0[position.row as usize][position.col as usize] != VISITED_CHAR {
-            self.0[position.row as usize][position.col as usize] = VISITED_CHAR;
-            return true;
-        }
-        false
-    }
-
     fn is_obstacle(&self, position: &Position) -> bool {
         self.0[position.row as usize][position.col as usize] == OBSTACLE_CHAR
     }
@@ -91,6 +84,86 @@ impl Grid {
     }
 }
 
+struct Runner<'a> {
+    grid: &'a Grid,
+    guard_position: Position,
+    guard_direction: Direction,
+    pub distinct_visited: i32,
+    pub visited_cells: HashMap<Position, HashSet<Direction>>,
+}
+
+impl<'a> Runner<'a> {
+    pub fn new(grid: &'a Grid,) -> Self {
+        Runner {
+            grid,
+            guard_position: grid.find_initial_guard_position(),
+            guard_direction: Direction::North,
+            distinct_visited: Default::default(),
+            visited_cells: Default::default(),
+        }
+    }
+
+    fn has_guard_position_been_visited(&self) -> bool {
+        self.visited_cells.contains_key(&self.guard_position)
+    }
+
+    fn mark_as_visited(&mut self) {
+        self.visited_cells
+            .entry(self.guard_position)
+            .or_default()
+            .insert(self.guard_direction);
+    }
+
+    fn find_next_valid_move(
+        &self,
+    ) -> Option<(Position, Direction)> {
+        let mut current_direction = self.guard_direction;
+
+        // try the 4 directions around the guard
+        for _ in 0..4 {
+            let next_position = Position::add(
+                &self.guard_position,
+                DIRECTIONS
+                    .get(&current_direction)
+                    .expect("No position movement for this direction"),
+            );
+
+            if !self.grid.is_valid_position(&next_position) {
+                return None;
+            }
+
+            if !self.grid.is_obstacle(&next_position) {
+                return Some((next_position, current_direction));
+            }
+
+            current_direction = DIRECTION_ROTATIONS
+                .get(&current_direction)
+                .expect("No rotation found for direction")
+                .clone();
+        }
+
+        return None;
+    }
+
+    pub fn run(&mut self) {
+        while self.grid.is_valid_position(&self.guard_position) {
+            if !self.has_guard_position_been_visited() {
+                self.distinct_visited += 1;
+            }
+            self.mark_as_visited();
+
+            if let Some((next_position, next_direction)) =
+                self.find_next_valid_move()
+            {
+                self.guard_position = next_position;
+                self.guard_direction = next_direction;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 fn init_grid() -> Grid {
     let lines = read_input("day6", "input.txt").unwrap();
 
@@ -102,60 +175,11 @@ fn init_grid() -> Grid {
     )
 }
 
-fn find_next_valid_move(
-    current: &Position,
-    direction: Direction,
-    grid: &Grid,
-) -> Option<(Position, Direction)> {
-    let mut current_direction = direction;
-
-    // try the 4 directions around the guard
-    for _ in 0..4 {
-        let next_position = Position::add(
-            &current,
-            DIRECTIONS
-                .get(&current_direction)
-                .expect("No position movement for this direction"),
-        );
-
-        if !grid.is_valid_position(&next_position) {
-            return None;
-        }
-
-        if !grid.is_obstacle(&next_position) {
-            return Some((next_position, current_direction));
-        }
-
-        current_direction = DIRECTION_ROTATIONS
-            .get(&current_direction)
-            .expect("No rotation found for direction")
-            .clone();
-    }
-
-    return None;
-}
-
 pub fn run_part_1() {
-    let mut grid = init_grid();
+    let grid = init_grid();
 
-    let mut distinct_visited = 0;
-    let mut guard_position = grid.find_initial_guard_position();
-    let mut guard_direction = Direction::North;
+    let mut runner = Runner::new(&grid);
+    runner.run();
 
-    while grid.is_valid_position(&guard_position) {
-        if grid.mark_cell_as_visited_if_not(&guard_position) {
-            distinct_visited += 1;
-        }
-
-        if let Some((next_position, next_direction)) =
-            find_next_valid_move(&guard_position, guard_direction, &grid)
-        {
-            guard_position = next_position;
-            guard_direction = next_direction;
-        } else {
-            break;
-        }
-    }
-
-    println!("{distinct_visited}");
+    println!("{}", runner.distinct_visited);
 }
