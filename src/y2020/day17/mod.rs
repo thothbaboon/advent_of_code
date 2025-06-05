1506f13cebea7c30d9a89c4bdfb47966047266a8
antoine <antoine>
2025-06-05 22:19:39 +0200

    aoc 2020 day 17 faster

--- src/y2020/day17/mod.rs
+++ src/y2020/day17/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::read_input;
 
@@ -35,44 +35,32 @@ pub fn run_part_1() {
     let mut state = read_initial_3d_state();
 
     for _ in 0..NB_CYCLES {
-        let mut cubes: HashSet<(isize, isize, isize)> = HashSet::<(isize, isize, isize)>::new();
+        let mut cubes_active_neighbors: HashMap<(isize, isize, isize), usize> = HashMap::new();
 
         for active_cube in &state {
             for x in [-1, 0, 1] {
                 for y in [-1, 0, 1] {
                     for z in [-1, 0, 1] {
-                        cubes.insert((active_cube.0 + x, active_cube.1 + y, active_cube.2 + z));
-                    }
-                }
-            }
-        }
-
-        let mut next_state = HashSet::new();
-        for cube in cubes {
-            let mut c_active_neighbors = 0;
-            for x in [-1, 0, 1] {
-                for y in [-1, 0, 1] {
-                    for z in [-1, 0, 1] {
-                        if !(x == 0 && y == 0 && z == 0)
-                            && state.contains(&(cube.0 + x, cube.1 + y, cube.2 + z))
-                        {
-                            c_active_neighbors += 1;
+                        if !(x == 0 && y == 0 && z == 0) {
+                            *cubes_active_neighbors
+                                .entry((active_cube.0 + x, active_cube.1 + y, active_cube.2 + z))
+                                .or_default() += 1;
                         }
                     }
                 }
             }
-
-            let is_active = state.contains(&cube);
-            if is_active {
-                if c_active_neighbors == 2 || c_active_neighbors == 3 {
-                    next_state.insert(cube);
-                }
-            } else if c_active_neighbors == 3 {
-                next_state.insert(cube);
-            }
         }
 
-        state = next_state;
+        state = cubes_active_neighbors
+            .iter()
+            .filter_map(|(k, v)| {
+                if *v == 3 || (state.contains(k) && *v == 2) {
+                    Some(*k)
+                } else {
+                    None
+                }
+            })
+            .collect::<HashSet<(isize, isize, isize)>>();
     }
 
     assert_eq!(state.len(), 319);
@@ -82,54 +70,40 @@ pub fn run_part_2() {
     let mut state = read_initial_4d_state();
 
     for _ in 0..NB_CYCLES {
-        let mut cubes: HashSet<(isize, isize, isize, isize)> =
-            HashSet::<(isize, isize, isize, isize)>::new();
+        let mut cubes_active_neighbors: HashMap<(isize, isize, isize, isize), usize> =
+            HashMap::new();
 
         for active_cube in &state {
             for x in [-1, 0, 1] {
                 for y in [-1, 0, 1] {
                     for z in [-1, 0, 1] {
                         for w in [-1, 0, 1] {
-                            cubes.insert((
-                                active_cube.0 + x,
-                                active_cube.1 + y,
-                                active_cube.2 + z,
-                                active_cube.3 + w,
-                            ));
-                        }
-                    }
-                }
-            }
-        }
-
-        let mut next_state = HashSet::new();
-        for cube in cubes {
-            let mut c_active_neighbors = 0;
-            for x in [-1, 0, 1] {
-                for y in [-1, 0, 1] {
-                    for z in [-1, 0, 1] {
-                        for w in [-1, 0, 1] {
-                            if !(x == 0 && y == 0 && z == 0 && w == 0)
-                                && state.contains(&(cube.0 + x, cube.1 + y, cube.2 + z, cube.3 + w))
-                            {
-                                c_active_neighbors += 1;
+                            if !(x == 0 && y == 0 && z == 0 && w == 0) {
+                                *cubes_active_neighbors
+                                    .entry((
+                                        active_cube.0 + x,
+                                        active_cube.1 + y,
+                                        active_cube.2 + z,
+                                        active_cube.3 + w,
+                                    ))
+                                    .or_default() += 1;
                             }
                         }
                     }
                 }
             }
-
-            let is_active = state.contains(&cube);
-            if is_active {
-                if c_active_neighbors == 2 || c_active_neighbors == 3 {
-                    next_state.insert(cube);
-                }
-            } else if c_active_neighbors == 3 {
-                next_state.insert(cube);
-            }
         }
 
-        state = next_state;
+        state = cubes_active_neighbors
+            .iter()
+            .filter_map(|(k, v)| {
+                if *v == 3 || (state.contains(k) && *v == 2) {
+                    Some(*k)
+                } else {
+                    None
+                }
+            })
+            .collect::<HashSet<(isize, isize, isize, isize)>>();
     }
 
     assert_eq!(state.len(), 2324);
